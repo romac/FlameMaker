43c90a405c675b6063b46b9c17a86357d13f11b6
Arthur Passuello <arthur.passuello@epfl.ch>
2013-03-19 13:59:07 +0100

    Implement interface Transformation and class AffineTransformation.

--- src/ch/epfl/flamemaker/geometrie2d/AffineTransformation.java
+++ src/ch/epfl/flamemaker/geometrie2d/AffineTransformation.java
@@ -13,5 +13,29 @@ public class AffineTransformation implements Transformation {
 		this.f = f;
 	}
 	
+	public Point transformPoint(Point p) {
+		return new Point (this.a*p.x()+this.b*p.y()+this.c, this.d*p.x()+this.c*p.y()+this.f);
+	}
+	
+	static AffineTransformation newTranslation(double dx, double dy){
+		 return new AffineTransformation(1, 0, dx, 0, 1, dy);
+		
+	}
+	static AffineTransformation newRotation (double theta){
+		return new AffineTransformation(Math.cos(theta), -Math.sin(theta), 0, Math.sin(theta), Math.cos(theta), 0);
+	}
+	static AffineTransformation newScaling(double sx, double sy){
+		return new AffineTransformation(sx, 0, 0, 0, sy, 0);
+	}
+	static AffineTransformation newShearX(double sx){
+		return new AffineTransformation (1, sx, 0, 0, 1, 0);
+	}
+	static AffineTransformation newShearY(double sy){
+		return new AffineTransformation (1, 0, 0, sy, 1, 0);
+	}
+	static AffineTransformation IDENTITY (){
+		return new AffineTransformation (1, 0, 0, 0, 1,0);
+	}
+	
 	
 }
--- src/ch/epfl/flamemaker/geometrie2d/Transformation.java
+++ src/ch/epfl/flamemaker/geometrie2d/Transformation.java
@@ -3,6 +3,6 @@ package ch.epfl.flamemaker.geometrie2d;
 
 public interface Transformation {
 	
-	Point transformPoint(Point p);
+	public Point transformPoint(Point p);
 	
 }
