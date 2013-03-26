ebe9539c252797e5ee5fc567ea4539c8bfe6e6b9
Romain Ruetschi <romain.ruetschi@gmail.com>
2013-03-27 00:13:02 +0100

    Add methods Arrays.copyOf2DArray( int[][] array ) and Arrays.maxOf2DArray( int[][] array ).

--- src/ch/epfl/flamemaker/util/Arrays.java
+++ src/ch/epfl/flamemaker/util/Arrays.java
@@ -18,4 +18,37 @@ public class Arrays
 		return copy;
 	}
 	
+	public static int[][] copyOf2DArray( int[][] array )
+	{
+		int[][] copy = new int[ array.length ][ array[ 0 ].length ];
+		
+		for( int i = 0; i < array.length; i++ )
+        {
+	        for( int j = 0; j < array[ 0 ].length; j++ )
+            {
+	            copy[ i ][ j ] = array[ i ][ j ];
+            }
+        }
+		
+		return copy;
+	}
+
+	public static int maxOf2DArray( int[][] array )
+    {
+		int max = Integer.MIN_VALUE;
+		
+		for( int i = 0; i < array.length; i++ )
+        {
+	        for( int j = 0; j < array[ 0 ].length; j++ )
+            {
+	            if( array[ i ][ j ] > max )
+	            {
+	            	max = array[ i ][ j ];
+	            }
+            }
+        }
+		
+		return max;
+    }
+	
 }
