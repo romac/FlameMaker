3574df859139549e3af2c58250c6c8b4752a9648
Romain Ruetschi <romain.ruetschi@gmail.com>
2013-03-26 23:16:48 +0100

    Use Arrays.asList() instead of constructing an ArrayList by hand.
    Don't mind the stupid whitespace shit. Fuck I hate Eclipse…

--- src/ch/epfl/flamemaker/ifs/IFSMaker.java
+++ src/ch/epfl/flamemaker/ifs/IFSMaker.java
@@ -2,6 +2,9 @@ package ch.epfl.flamemaker.ifs;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
 import ch.epfl.flamemaker.pbm.*;
 
 import ch.epfl.flamemaker.geometrie2d.AffineTransformation;
@@ -11,48 +14,40 @@ import ch.epfl.flamemaker.geometrie2d.Rectangle;
 public class IFSMaker
 {
 
-	
-	public static void main(String[] args)
-	{
-		IFSMaker.sierpinsky();
-		IFSMaker.barnsley();
-	}
-	
-	private static void barnsley()
-	{
-		ArrayList<AffineTransformation> transformations = new ArrayList<AffineTransformation>();
-		
-		AffineTransformation A1 = new AffineTransformation(
-		    0, 0,   0,
-		    0, 0.16, 0
-		);
-		
-		AffineTransformation A2 = new AffineTransformation(
-	        0.2, -0.26, 0,
-	        0.23, 0.22, 1.6
-	    );
-		
-		AffineTransformation A3 = new AffineTransformation(
-	        -0.15, 0.28, 0,
-	        0.26, 0.24, 0.44
-	    );
-		
-		AffineTransformation A4 = new AffineTransformation(
-			0.85,  0.04, 0,
-			-0.04, 0.85, 1.6
-		);
-		
-		transformations.add( A1 );
-		transformations.add( A2 );
-		transformations.add( A3 );
-		transformations.add( A4 );
-		
-		IFS ifs = new IFS( transformations );
-		Rectangle frame = new Rectangle( new Point( 0, 4.5 ), 6, 10 );
-		
-		IFSAccumulator accumulator = ifs.compute( frame, 120, 200, 150 );
-		
-		PBMWriter writer;
+    
+    public static void main(String[] args)
+    {
+        IFSMaker.sierpinsky();
+        IFSMaker.barnsley();
+    }
+    
+    private static void barnsley()
+    {
+        List<AffineTransformation> transformations = Arrays.asList(
+            new AffineTransformation(
+                0, 0,   0,
+                0, 0.16, 0
+            ),  
+            new AffineTransformation(
+                0.2, -0.26, 0,
+                0.23, 0.22, 1.6
+            ),      
+            new AffineTransformation(
+                -0.15, 0.28, 0,
+                0.26, 0.24, 0.44
+            ),      
+            new AffineTransformation(
+                0.85,  0.04, 0,
+                -0.04, 0.85, 1.6
+            )
+        );
+        
+        IFS ifs = new IFS( transformations );
+        Rectangle frame = new Rectangle( new Point( 0, 4.5 ), 6, 10 );
+        
+        IFSAccumulator accumulator = ifs.compute( frame, 120, 200, 150 );
+        
+        PBMWriter writer;
         try {
             writer = new PBMWriter( "resources/barnsley.pbm" );
             writer.printAccumulator( accumulator, true );
@@ -60,37 +55,31 @@ public class IFSMaker
         catch( FileNotFoundException e ) {
             e.printStackTrace();
         }
-	}
-	
-	private static void sierpinsky()
-	{
-		ArrayList<AffineTransformation> transformations = new ArrayList<AffineTransformation>();
-		
-		AffineTransformation A1 = new AffineTransformation(
-		    0.5, 0, 0,
-		    0, 0.5, 0
-		);
-		
-		AffineTransformation A2 = new AffineTransformation(
-	        0.5, 0, 0.5,
-	        0, 0.5, 0
-	    );
-		
-		AffineTransformation A3 = new AffineTransformation(
-	        0.5, 0, 0.25,
-	        0, 0.5, 0.5
-	    );
-		
-		transformations.add( A1 );
-		transformations.add( A2 );
-		transformations.add( A3 );
-		
-		IFS ifs = new IFS( transformations );
-		Rectangle frame = new Rectangle( new Point( .5, .5 ), 1, 1 );
-		
-		IFSAccumulator accumulator = ifs.compute( frame, 100, 100, 1 );
-		
-		PBMWriter writer;
+    }
+    
+    private static void sierpinsky()
+    {
+        List<AffineTransformation> transformations = Arrays.asList(
+            new AffineTransformation(
+                0.5, 0, 0,
+                0, 0.5, 0
+            ),
+            new AffineTransformation(
+                0.5, 0, 0.5,
+                0, 0.5, 0
+            ),
+            new AffineTransformation(
+                0.5, 0, 0.25,
+                0, 0.5, 0.5
+            )
+        );
+        
+        IFS ifs = new IFS( transformations );
+        Rectangle frame = new Rectangle( new Point( .5, .5 ), 1, 1 );
+        
+        IFSAccumulator accumulator = ifs.compute( frame, 100, 100, 1 );
+        
+        PBMWriter writer;
         try {
             writer = new PBMWriter( "resources/sierpinski.pbm" );
             writer.printAccumulator( accumulator, true );
@@ -98,6 +87,6 @@ public class IFSMaker
         catch( FileNotFoundException e ) {
             e.printStackTrace();
         }
-	}
+    }
 
 }
