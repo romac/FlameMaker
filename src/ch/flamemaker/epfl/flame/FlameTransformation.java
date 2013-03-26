9ebf9c3a00354b56b7f715797c9b50f998d63569
Romain Ruetschi <romain.ruetschi@gmail.com>
2013-03-26 23:43:32 +0100

    Add class ch.flamemaker.epfl.flame.FlameTransformation.

---
+++ src/ch/flamemaker/epfl/flame/FlameTransformation.java
@@ -0,0 +1,43 @@
+package ch.flamemaker.epfl.flame;
+
+import ch.epfl.flamemaker.geometrie2d.AffineTransformation;
+import ch.epfl.flamemaker.geometrie2d.Point;
+import ch.epfl.flamemaker.geometrie2d.Transformation;
+
+public class FlameTransformation implements Transformation
+{
+
+	private AffineTransformation affineTransformation;
+	private double[] variationWeight;
+
+	FlameTransformation( AffineTransformation affineTransformation, double[] variationWeight )
+	{
+		if( variationWeight.length != Variation.ALL_VARIATIONS.size() )
+		{
+			throw new IllegalArgumentException(
+				"variationWeight should be of length " + Variation.ALL_VARIATIONS.size() + "."
+			);
+		}
+		
+		this.affineTransformation = affineTransformation;
+		this.variationWeight = variationWeight.clone();
+	}
+
+	@Override
+	public Point transformPoint( Point p )
+	{
+		Point r = new Point( 0, 0 );
+		
+		for( int j = 0; j < Variation.ALL_VARIATIONS.size(); j++ )
+		{
+			Point g =  this.affineTransformation.transformPoint( p );
+			Point v = Variation.ALL_VARIATIONS.get( j ).transformPoint( g );
+			Point f = v.scale( this.variationWeight[ j ] );
+			
+			r = new Point( r.x() + f.x(), r.y() + f.y() );
+		}
+		
+		return r;
+	}
+
+}
