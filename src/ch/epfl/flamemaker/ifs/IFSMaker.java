330f3910db6c02b2d27867e014f1ef6005804f91
Romain Ruetschi <romain.ruetschi@gmail.com>
2013-03-26 22:35:35 +0100

    Add Barnsley fractal.

--- src/ch/epfl/flamemaker/ifs/IFSMaker.java
+++ src/ch/epfl/flamemaker/ifs/IFSMaker.java
@@ -14,22 +14,71 @@ public class IFSMaker
 	
 	public static void main(String[] args)
 	{
+		IFSMaker.sierpinsky();
+		IFSMaker.barnsley();
+	}
+
+	private static void barnsley()
+	{
+		ArrayList<AffineTransformation> transformations = new ArrayList<AffineTransformation>();
+
+		AffineTransformation A1 = new AffineTransformation(
+		    0, 0,   0,
+		    0, 0.16, 0
+		);
+
+		AffineTransformation A2 = new AffineTransformation(
+	        0.2, -0.26, 0,
+	        0.23, 0.22, 1.6
+	    );
+
+		AffineTransformation A3 = new AffineTransformation(
+	        -0.15, 0.28, 0,
+	        0.26, 0.24, 0.44
+	    );
+
+		AffineTransformation A4 = new AffineTransformation(
+			0.85,  0.04, 0,
+			-0.04, 0.85, 1.6
+		);
 		
+		transformations.add( A1 );
+		transformations.add( A2 );
+		transformations.add( A3 );
+		transformations.add( A4 );
+
+		IFS ifs = new IFS( transformations );
+		Rectangle frame = new Rectangle( new Point( 0, 4.5 ), 6, 10 );
+
+		IFSAccumulator accumulator = ifs.compute( frame, 120, 200, 150 );
+
+		PBMWriter writer;
+        try {
+            writer = new PBMWriter( "resources/barnsley.pbm" );
+            writer.printAccumulator( accumulator, true );
+        }
+        catch( FileNotFoundException e ) {
+            e.printStackTrace();
+        }
+	}
+
+	private static void sierpinsky()
+	{
 		ArrayList<AffineTransformation> transformations = new ArrayList<AffineTransformation>();
 		
 		AffineTransformation A1 = new AffineTransformation(
-		    .5, 0, 0,
-		    0, .5, 0
+		    0.5, 0, 0,
+		    0, 0.5, 0
 		);
 		
 		AffineTransformation A2 = new AffineTransformation(
-	        .5, 0, .5,
-	        0, .5, 0
+	        0.5, 0, 0.5,
+	        0, 0.5, 0
 	    );
 		
 		AffineTransformation A3 = new AffineTransformation(
-	        .5, 0, .25,
-	        0, .5, .5
+	        0.5, 0, 0.25,
+	        0, 0.5, 0.5
 	    );
 		
 		transformations.add( A1 );
