5cff285d43b31ae8173fa0b9756cdee4ea948c41
Romain Ruetschi <romain.ruetschi@gmail.com>
2013-05-08 15:05:46 +0200

    Add a GUI, with a special component previewing the fractal.

---
+++ src/ch/epfl/flamemaker/gui/FlameBuilderPreviewComponent.java
@@ -0,0 +1,78 @@
+package ch.epfl.flamemaker.gui;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JComponent;
+
+import ch.epfl.flamemaker.color.Color;
+import ch.epfl.flamemaker.color.Palette;
+import ch.epfl.flamemaker.flame.Flame;
+import ch.epfl.flamemaker.flame.FlameAccumulator;
+import ch.epfl.flamemaker.flame.Flame.Builder;
+import ch.epfl.flamemaker.geometry2d.Rectangle;
+
+public class FlameBuilderPreviewComponent extends JComponent
+{
+
+	private static final long serialVersionUID = 4246053641524095098L;
+	private Flame.Builder builder;
+	private Color bgColor;
+	private Palette palette;
+	private Rectangle frame;
+	private int density;
+
+	public FlameBuilderPreviewComponent( Flame.Builder builder, Color bgColor, Palette palette, Rectangle frame, int density )
+	{
+		this.builder = builder;
+		this.bgColor = bgColor;
+		this.palette = palette;
+		this.frame = frame;
+		this.density = density;
+	}
+	
+	@Override
+	protected void paintComponent( Graphics g0 )
+	{
+		Graphics2D g = ( Graphics2D )g0;
+		
+		System.out.println( "Component: " + this.getWidth() + ", " + this.getHeight() );
+		double aspectRatio = ( double )this.getWidth() / ( double )this.getHeight();
+		System.out.println( "Component aspect ratio: " + aspectRatio );
+		System.out.println( "Frame: " + frame );
+		Rectangle expandedFrame = frame.expandToAspectRatio( aspectRatio );
+		System.out.println( "Expanded frame : " + expandedFrame );
+		BufferedImage image = new BufferedImage( this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_RGB );
+		
+		Flame flame = this.builder.build();
+		FlameAccumulator accumulator = flame.compute( expandedFrame, this.getWidth(), this.getHeight(), this.density );
+		
+		for( int i = 0; i < accumulator.width(); i++ )
+		{
+			for( int j = 0; j < accumulator.height(); j++ )
+			{
+				Color color = accumulator.color( this.palette, this.bgColor, i, j );
+				image.setRGB( i, accumulator.height() - j - 1, color.asPackedRGB() );
+			}
+		}
+		
+		g.drawImage( image, 0, 0, null );
+	}
+	
+	@Override
+	public Dimension getPreferredSize()
+	{
+		Dimension parentSize = this.getParent().getSize();
+		
+		if( parentSize.getWidth() < 200 || parentSize.getHeight() < 100 )
+		{
+			return new Dimension( 500, 400 );
+		}
+		
+		return new Dimension( (int)parentSize.getWidth() - 20, (int)parentSize.getHeight() - 35 );
+	}
+	
+	
+}
---
+++ src/ch/epfl/flamemaker/gui/FlameMakerGUI.java
@@ -0,0 +1,93 @@
+package ch.epfl.flamemaker.gui;
+
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+import ch.epfl.flamemaker.color.Color;
+import ch.epfl.flamemaker.color.InterpolatedPalette;
+import ch.epfl.flamemaker.color.Palette;
+import ch.epfl.flamemaker.flame.Flame;
+import ch.epfl.flamemaker.flame.FlameTransformation;
+import ch.epfl.flamemaker.geometry2d.AffineTransformation;
+import ch.epfl.flamemaker.geometry2d.Rectangle;
+import ch.epfl.flamemaker.geometry2d.Point;
+
+public class FlameMakerGUI
+{
+	
+	private Flame.Builder builder 	= new Flame.Builder( null );
+	private Color bgColor 			= Color.BLACK;
+	private Palette palette 		= InterpolatedPalette.RGB_PALETTE;
+	private Rectangle frame 		= new Rectangle( new Point( -0.25, 0 ), 5, 4 );
+	private int density 			= 50;
+	
+	public void start()
+    {
+		JFrame frame = new JFrame( "Flame Maker" );
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		
+		Container contentPane = frame.getContentPane(); 
+		contentPane.setLayout( new BorderLayout() );
+		
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout( new GridLayout( 1, 2 ) );
+		
+		GridBagConstraints fillConstraints = new GridBagConstraints();
+		fillConstraints.fill = GridBagConstraints.BOTH;
+		
+		JPanel transformationsPanel = new JPanel();
+		transformationsPanel.setBorder( BorderFactory.createTitledBorder( "Transformations affines" ) );
+		
+		JPanel fractalPanel = new JPanel();
+		fractalPanel.setBorder( BorderFactory.createTitledBorder( "Fractale" ) );
+		
+		topPanel.add( transformationsPanel, fillConstraints );
+		topPanel.add( fractalPanel, fillConstraints );
+		
+		transformationsPanel.add( new JLabel( "Hello, World" ) );
+		
+		fractalPanel.add( this.getPreview() );
+		
+		contentPane.add( topPanel, BorderLayout.CENTER );
+		
+		frame.pack();
+		frame.setVisible( true );
+    }
+	
+	private FlameBuilderPreviewComponent getPreview()
+	{
+		
+		FlameBuilderPreviewComponent preview = new FlameBuilderPreviewComponent(
+			this.builder, this.bgColor, this.palette, this.frame, this.density
+		);
+		
+		this.builder.addTransformation( new FlameTransformation(
+            new AffineTransformation(
+            	-0.4113504, -0.7124804, -0.4,
+            	 0.7124795, -0.4113508,  0.8
+            ),
+            new double[] { 1, 0.1, 0, 0, 0, 0 }
+        ) );
+		
+		this.builder.addTransformation( new FlameTransformation(
+            new AffineTransformation(
+                -0.3957339,  0, 		-1.6,
+                 0, 		-0.3957337,  0.2
+            ),
+            new double[] { 0, 0, 0, 0, 0.8, 1 }
+       ) );
+		
+		this.builder.addTransformation( new FlameTransformation(
+            new AffineTransformation(
+            	0.4810169, 0, 		  1,
+                0, 		   0.4810169, 0.9
+            ),
+            new double[] { 1, 0, 0, 0, 0, 0 }
+        ) );
+		
+		return preview;
+	}
+	
+}
